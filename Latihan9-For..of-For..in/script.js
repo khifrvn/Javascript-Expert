865d1a7c1214cd6e3eeddefa19a38085898c7120
Khifransyah <96522082+khifrvn@users.noreply.github.com>
2022-02-01 17:45:48 +0700

    Add Latihan9

---
+++ Latihan9-For..of-For..in/script.js
@@ -0,0 +1,58 @@
+// forEach -> looping untuk dan khusus Array
+
+// ---------- For of ----------------
+
+/* for..of -> menelusuri object iterable 
+ - String
+ - Array
+ - Arguments / NodeList
+ - TypedArray
+ - Map
+ - Set
+ - User-defind iterables   
+*/ 
+
+// Array
+const mhs = ['Edo', 'Edi', 'Edu'];
+
+// mhs.forEach(i => console.log(i));  --> For Each
+
+for(const i of mhs){  // Penggunaan for..of
+    console.log(i);
+}
+
+// String
+const nama = 'Edo';
+for(const i of nama){
+    console.log(i)
+}
+
+
+// NodeList
+const namaMhs = document.querySelectorAll('.nama');
+
+for (let i of namaMhs){
+    console.log(i.innerHTML);
+}
+
+// arguments
+function jumlahkanAngka(){
+    let jumlah = 0;
+    for(let i of arguments){
+        jumlah += i;
+    }
+    return jumlah;
+}
+
+jumlahkanAngka(1,2,3,4,5);
+
+//  --------- FOR..IN ------------
+// on Object
+const mhs = {
+    nama: 'Edo',
+    umur: 22,
+}
+
+for(i in mhs){
+    console.log(i);
+}
